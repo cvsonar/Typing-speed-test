82abecc276c22eb42acd68de0741aeda2af3d052
chetan sonar <109953587+cvsonar@users.noreply.github.com>
2024-07-15 19:20:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import SpeedTypingGame from './components/speedTypingGame/SpeedTypingGame';
+
+function App() {
+  return (
+    <div className="App m-5">
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default App;
